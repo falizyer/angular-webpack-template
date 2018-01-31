751ada1be87994df1a3d30da0e8f9c66612c205e
Dzmitry Hrachou <dhrachou@slb.com>
2018-01-31 17:39:22 +0300

    angular has been added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
 import "./app.style.less";
 
+import {bootstrap, element, module} from "angular";
 
+module("template", [])
+    .component("tplApp", {
+
+    });
+
+element(document)
+    .ready(() => {
+        bootstrap(document, ["template"]);
+    });
