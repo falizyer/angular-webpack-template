f928e729277fa1eb5a0d42f1a615cddf5dc67a47
Dzmitry Hrachou <dhrachou@slb.com>
2018-01-31 17:05:02 +0300

    eslint has been added

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: "babel",
+  "plugins": [
+  ],
+  rules: {
+    "no-var": 0,
+    "max-len": 0
+  },
+  env: {
+    node: true,
+    mocha: true
+  }
+};
