b1c98754ca0ef2bb198fcdafbefa77850ffd653c
Dzmitry Hrachou <dhrachou@slb.com>
2018-02-05 17:20:32 +0300

    Integrating karma

--- config/karma.config.js
+++ config/karma.config.js
@@ -3,20 +3,24 @@ const webpackConfig = require("./webpack.config");
 module.exports = function (config) {
     config.set({
         //root path location to resolve paths defined in files and exclude
-        basePath: '../../',
+        basePath: "../",
         //files/patterns to exclude from loaded files
         exclude: [],
         //files/patterns to load in the browser
         files: [
-            {pattern: 'test/*.spec.js', watched: true, served: true, included: true}
+            "node_modules/angular/angular.js",
+            "node_modules/angular-mocks/angular-mocks.js",
+            "src/**/*.module.js",
+            "src/**/*.component.js",
+            {pattern: "test/*.js", watched: true, served: true, included: true}
             /*parameters*/
             //watched: if autoWatch is true all files that have set watched to true will be watched for changes
-            //served: should the files be served by Karma's webserver?
+            //served: should the files be served by Karma"s webserver?
             //included: should the files be included in the browser using <script> tag?
-            //nocache: should the files be served from disk on each request by Karma's webserver?
+            //nocache: should the files be served from disk on each request by Karma"s webserver?
             /*assets*/
-            //{pattern: '*.html', watched:true, served:true, included:false}
-            //{pattern: 'images/*', watched:false, served:true, included:false}
+            //{pattern: "*.html", watched:true, served:true, included:false}
+            //{pattern: "images/*", watched:false, served:true, included:false}
         ],
 
         webpack: webpackConfig,
@@ -31,16 +35,16 @@ module.exports = function (config) {
         logLevel: config.LOG_WARN, //config.LOG_DISABLE, config.LOG_ERROR, config.LOG_INFO, config.LOG_DEBUG
 
         //list of frameworks you want to use, only jasmine is installed automatically
-        frameworks: ['jasmine'],
+        frameworks: ["jasmine"],
         //list of browsers to launch and capture
-        browsers: ['PhantomJS'/*,'PhantomJS','Firefox','Edge','ChromeCanary','Opera','IE','Safari'*/],
+        browsers: ["PhantomJS"/*,"PhantomJS","Firefox","Edge","ChromeCanary","Opera","IE","Safari"*/],
         //list of reporters to use
-        reporters: ['mocha', 'kjhtml'/*,'dots','progress','spec'*/],
+        reporters: ["mocha", "kjhtml"/*,"dots","progress","spec"*/],
 
-        //address that the server will listen on, '0.0.0.0' is default
-        listenAddress: '0.0.0.0',
-        //hostname to be used when capturing browsers, 'localhost' is default
-        hostname: 'localhost',
+        //address that the server will listen on, "0.0.0.0" is default
+        listenAddress: "0.0.0.0",
+        //hostname to be used when capturing browsers, "localhost" is default
+        hostname: "localhost",
         //the port where the web server will be listening, 9876 is default
         port: 9876,
 
@@ -71,17 +75,18 @@ module.exports = function (config) {
             //use webpack to support require() in test-suits .js files
             //use babel-loader from webpack to compile es2015 features in .js files
             //add webpack as preprocessor
-            // 'src/mud-design/module.loader.es6.js': ['webpack'],
-            './test/*.js': ['webpack']
+            // "src/mud-design/module.loader.es6.js": ["webpack"],
+            "./test/*.js": ["webpack"],
+            "./src/**/*.js": ["webpack"]
         },
         webpackMiddleware: {
             //turn off webpack bash output when run the tests
             noInfo: true,
-            stats: 'errors-only'
+            stats: "errors-only"
         },
         /*karma-mocha-reporter config*/
         mochaReporter: {
-            output: 'noFailures'  //full, autowatch, minimal
+            output: "noFailures"  //full, autowatch, minimal
         }
     });
 };
